1ff3ba7573ebf88978fd7db7808a57b8f3ebe192
Yazeed7 <61038456+Yazeed7@users.noreply.github.com>
2020-03-12 22:36:34 -0700

    Create severity_nationwide.py

---
+++ src/severity_nationwide.py
@@ -0,0 +1,24 @@
+def is_highway(description):
+    '''
+    Determines from description whether or not an accident occured in a highway
+    :description: list of strings
+    '''
+    assert isinstance(description, list)
+    for x in description: assert isinstance(x,str)
+    out=[False]*len(description)
+    for i,line in enumerate(description):
+        if '-' in line:
+            out[i]=True
+    return out
+def get_day_of_week(time):
+    out=[]
+    for i in time:
+        out.append(i.strftime('%A'))
+    return out
+def get_highway_accidents_and_severity():
+    header,severity=import_data('US_Accidents_Dec19.csv',read='col',samples=None,col_num=3)
+    header,description=import_data('US_Accidents_Dec19.csv',read='col',samples=None,col_num=11)
+    header,start_time=import_data('US_Accidents_Dec19.csv',read='col',samples=None,col_num=4)
+    day_of_week=get_day_of_week(start_time)
+    highway=is_highway(description)
+    return pd.DataFrame({'Severity':severity,'Highway':highway,'Day of Week':day_of_week})
